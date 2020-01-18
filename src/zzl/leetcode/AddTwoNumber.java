01279e024b3c9ad41dd155118883bf507d87239e
superzhangzl <z553234736@outlook.com>
2020-01-18 17:45:10 +0800

    修复两数相加进位判断问题

--- src/zzl/leetcode/AddTwoNumber.java
+++ src/zzl/leetcode/AddTwoNumber.java
@@ -17,23 +17,31 @@ public class AddTwoNumber {
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
 
-        System.out.println(addTwoNumbers(l1, l2));
+//        System.out.println(addTwoNumbers(l1, l2));
+//
+//        System.out.println(addTwoNumbers(l1, null));
+//
+//        l2.next.next = null;
+//        System.out.println(addTwoNumbers(l1, l2));
+//
+//        l2.next.next = new ListNode(4);
+//        l2.next.next.next = new ListNode(1);
+//        System.out.println(addTwoNumbers(l1, l2));
+//
+//        l1 = new ListNode(4);
+//        l2 = null;
+//        System.out.println(addTwoNumbers(l1, l2));
+//
+//        l1 = null;
+//        l2 = null;
+//        System.out.println(addTwoNumbers(l1, l2));
 
-        System.out.println(addTwoNumbers(l1, null));
-
-        l2.next.next = null;
-        System.out.println(addTwoNumbers(l1, l2));
-
-        l2.next.next = new ListNode(4);
-        l2.next.next.next = new ListNode(1);
-        System.out.println(addTwoNumbers(l1, l2));
-
-        l1 = new ListNode(4);
-        l2 = null;
-        System.out.println(addTwoNumbers(l1, l2));
-
-        l1 = null;
-        l2 = null;
+//        l1 = new ListNode(5);
+//        l2 = new ListNode(5);
+//        System.out.println(addTwoNumbers(l1, l2));
+        l1 = new ListNode(1);
+        l2 = new ListNode(9);
+        l2.next = new ListNode(9);
         System.out.println(addTwoNumbers(l1, l2));
     }
 
@@ -44,28 +52,29 @@ public class AddTwoNumber {
         }
         // 表示进位的flag，初始化为0
         int flag = 0;
-        ListNode indexL1 = l1;
-        ListNode indexL2 = l2;
         // 初始化任意一个值，只是为了不出现NPE
         ListNode result = new ListNode(-1);
         ListNode resultIndex = result;
-        while (indexL1 != null || indexL2 != null) {
+        while (l1 != null || l2 != null) {
             // 若长度不一致，差的就补全为0
-            if (indexL1 == null) {
-                indexL1 = new ListNode(0);
+            if (l1 == null) {
+                l1 = new ListNode(0);
             }
-            if (indexL2 == null) {
-                indexL2 = new ListNode(0);
+            if (l2 == null) {
+                l2 = new ListNode(0);
             }
             // todo assert  0 <= val < 10
-            int currentValue = indexL1.val + indexL2.val;
+            int currentValue = (l1.val + l2.val + flag);
             // 当前节点之和加上一级的进位
-            ListNode currentNode = new ListNode(currentValue % 10 + flag);
+            ListNode currentNode = new ListNode(currentValue % 10);
             // 计算当前节点需要向下一级node进位的值
             flag = currentValue / 10;
             resultIndex.next = currentNode;
-            indexL1 = indexL1.next;
-            indexL2 = indexL2.next;
+            if (flag > 0) {
+                currentNode.next = new ListNode(flag);
+            }
+            l1 = l1.next;
+            l2 = l2.next;
             resultIndex = resultIndex.next;
         }
         // 从第二个节点开始返回
