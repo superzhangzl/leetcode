5a4590a5ad7592226a8f3e6f38b63b36b8bf4e5e
superzhangzl <z553234736@outlook.com>
2020-01-18 21:12:54 +0800

    add

---
+++ src/zzl/leetcode/LongestPalindrome.java
@@ -0,0 +1,29 @@
+package zzl.leetcode;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ * <p>
+ * 输入: "cbbd"
+ * 输出: "bb"
+ *
+ * @author zzl
+ * @link {https://leetcode-cn.com/problems/longest-palindromic-substring}
+ */
+public class LongestPalindrome {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestPalindrome(String s) {
+
+        return null;
+    }
+}
