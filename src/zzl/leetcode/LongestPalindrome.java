7166068ee4e5a8217389b7c9e2d7c99675e3d980
张泽龙 <553234736@qq.com>
2020-01-19 19:23:37 +0800

    添加 最大回文子串

--- src/zzl/leetcode/LongestPalindrome.java
+++ src/zzl/leetcode/LongestPalindrome.java
@@ -19,11 +19,93 @@ package zzl.leetcode;
 public class LongestPalindrome {
 
     public static void main(String[] args) {
-
+        System.out.println(longestPalindrome("babad"));
+        System.out.println(longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome("c"));
+        System.out.println(longestPalindrome(""));
+        System.out.println(longestPalindrome(null));
+        System.out.println(longestPalindrome("null"));
+        System.out.println(longestPalindrome("abcba"));
+        System.out.println(longestPalindrome("abc"));
+        System.out.println(longestPalindrome("ab"));
+        System.out.println(longestPalindrome("aa"));
+        System.out.println(longestPalindrome("aaa"));
+        System.out.println(longestPalindrome("aba"));
+        System.out.println(longestPalindrome("aab"));
+        System.out.println(longestPalindrome("baa"));
+        System.out.println(longestPalindrome("aabc"));
     }
 
     public static String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        // 长度为1直接返回
+        if (s.length() == 1) {
+            return s;
+        }
+        int right = 0;
+        int left = 0;
+        int maxLength = 0;
+        // 思路：从当前位置开始，向两边同时遍历判断
+        for (int i = 0; i < s.length(); i++) {
+            char middleChar = s.charAt(i);
+            print(i);
+            print(middleChar);
+            // 判断回文序列为奇数的情况
+            int leftIndex = i;
+            int rightIndex = i;
+            while (true) {
+                if ((leftIndex >= 0) && (rightIndex < s.length()) && s.charAt(leftIndex) == s.charAt(rightIndex)) {
+                    print("current: " + s.substring(leftIndex, rightIndex + 1));
+                    rightIndex++;
+                    leftIndex--;
+                } else {
+                    break;
+                }
+            }
+            int length = (rightIndex - 1) - (leftIndex + 1) + 1;
+            if (length > maxLength) {
+                if (length == s.length()) {
+                    return s;
+                }
+                maxLength = length;
+                left = leftIndex + 1;
+                right = rightIndex - 1;
+                print("max length: " + length);
+                print("left, right: " + left + ", " + right);
+                print("max str: " + s.substring(left, right + 1));
+            }
+            // 判断回文序列为偶数的情况，区别就是右游标多移动一位而已
+            leftIndex = i;
+            rightIndex = i + 1;
+            while (true) {
+                if ((leftIndex >= 0) && (rightIndex < s.length()) && s.charAt(leftIndex) == s.charAt(rightIndex)) {
+                    print("current: " + s.substring(leftIndex, rightIndex + 1));
+                    rightIndex++;
+                    leftIndex--;
+                } else {
+                    break;
+                }
+            }
+            length = (rightIndex - 1) - (leftIndex + 1) + 1;
+            if (length > maxLength) {
+                if (length == s.length()) {
+                    return s;
+                }
+                maxLength = length;
+                left = leftIndex + 1;
+                right = rightIndex - 1;
+                print("max length: " + length);
+                print("left, right: " + left + ", " + right);
+                print("max str: " + s.substring(left, right + 1));
+            }
+            print("============================");
+        }
+        return s.substring(left, right + 1);
+    }
 
-        return null;
+    private static void print(Object msg) {
+//        System.out.println(msg);
     }
 }
