a2294a2ef2745ef5044cd6701d510c78d47cc9d6
张泽龙 <553234736@qq.com>
2020-03-02 09:21:46 +0800

    更新  	求根到叶子节点数字之和(直接数乘，不使用sb来保存堆栈)

--- src/zzl/leetcode/SumRootToLeafNumbers.java
+++ src/zzl/leetcode/SumRootToLeafNumbers.java
@@ -41,7 +41,8 @@ public class SumRootToLeafNumbers {
      * @return
      */
     public int sumNumbers(TreeNode root) {
-        dfs(root, new StringBuilder());
+        dfs2(root, 0);
+//        dfs(root, new StringBuilder());
         return result;
     }
 
@@ -61,5 +62,26 @@ public class SumRootToLeafNumbers {
             result += Integer.parseInt(stack.toString());
         }
         stack.deleteCharAt(stack.length() - 1);
+    }
+
+    /**
+     * 核心：当前节点值 + 10 * 之前的值
+     *
+     * @param root
+     * @param cur
+     */
+    private void dfs2(TreeNode root, int cur) {
+        cur = cur * 10 + root.val;
+        if (root == null) {
+            return;
+        }
+        // 先序遍历
+        if (root.left != null || root.right != null) {
+            dfs2(root.left, cur);
+            dfs2(root.right, cur);
+        } else {
+            // 打印由前序遍历的来的，所有的root节点到叶子节点的路径
+            result += cur;
+        }
     }
 }
