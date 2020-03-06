0a136bd73e0a9ccae7c55cc781994efb88a795a9
张泽龙 <553234736@qq.com>
2020-03-06 08:23:07 +0800

    添加  	加油站(O(n) )

--- src/zzl/leetcode/GasStation.java
+++ src/zzl/leetcode/GasStation.java
@@ -20,7 +20,44 @@ public class GasStation {
 
     }
 
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+    /**
+     * @param gas
+     * @param cost
+     * @return
+     * @link {https://leetcode-cn.com/problems/gas-station/solution/jia-you-zhan-by-leetcode/}
+     */
+    private int canCompleteCircuit(int[] gas, int[] cost) {
+        int length = gas.length;
+        // 总的储油量
+        int totalTank = 0;
+        // 当前起点的储油量
+        int currentTank = 0;
+        // 记录起始位置
+        int startStation = 0;
+        for (int i = 0; i < length; i++) {
+            totalTank += gas[i] - cost[i];
+            currentTank += gas[i] - cost[i];
+            // 如果从startStation开始的形成油量不够，那就从下一个位置开始
+            // 把当前的油量置为0
+            // 但总的total继续累加，只有总的剩余油量大于等于0，说明才能返回起点
+            // 只要从startStation~length可达，那从0~startStation也可达，证明过程详见链接
+            if (currentTank < 0) {
+                startStation = i + 1;
+                currentTank = 0;
+            }
+
+        }
+        return totalTank >= 0 ? startStation : -1;
+    }
+
+    /**
+     * 最基础的第一想法，但是时间复杂度高，O(n^2)
+     *
+     * @param gas
+     * @param cost
+     * @return
+     */
+    public int canCompleteCircuitStupid(int[] gas, int[] cost) {
         int length = gas.length;
         // 有先行条件，就不特殊判断了
         for (int i = 0; i < length; i++) {
@@ -38,6 +75,7 @@ public class GasStation {
                 capacity += gas[index];
                 // 满足条件就执行，不满足就跳出当前循环
                 if (capacity < cost[index]) {
+                    i = index + 1;
                     break;
                 } else {
                     capacity -= cost[index];
