440d454408363d6e470e786391a645a8d63888c9
张泽龙 <553234736@qq.com>
2020-03-06 10:53:06 +0800

    添加  	数组中的第K个最大元素(堆)

--- src/zzl/leetcode/KthLargestElementInAnArray.java
+++ src/zzl/leetcode/KthLargestElementInAnArray.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import zzl.util.PrintConsoleUtil;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 /**
  * 数组中的第K个最大元素
@@ -15,8 +16,34 @@ import java.util.Arrays;
 public class KthLargestElementInAnArray {
     public static void main(String[] args) {
 
-        Assert.assertEquals(new KthLargestElementInAnArray().findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4), 4);
-        Assert.assertEquals(new KthLargestElementInAnArray().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2), 5);
+        Assert.assertEquals(new KthLargestElementInAnArray().findKthLargestBetter(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4), 4);
+        Assert.assertEquals(new KthLargestElementInAnArray().findKthLargestBetter(new int[]{3, 2, 1, 5, 6, 4}, 2), 5);
+    }
+
+    /**
+     * 使用最大堆保存前k大个数，最后抛出最后一个
+     * 时间复杂度 : {O}(N \log k)O(Nlogk)。
+     * 空间复杂度 : {O}(k)O(k)，用于存储堆元素
+     *
+     * @param nums
+     * @param k
+     * @return
+     * @link {https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/shu-zu-zhong-de-di-kge-zui-da-yuan-su-by-leetcode/}
+     */
+    public int findKthLargestBetter(int[] nums, int k) {
+        // init heap 'the smallest element first'
+        PriorityQueue<Integer> heap =
+                new PriorityQueue<>((n1, n2) -> n1 - n2);
+
+        // keep k largest elements in the heap
+        for (int n : nums) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        // output
+        return heap.poll();
     }
 
     public int findKthLargest(int[] nums, int k) {
