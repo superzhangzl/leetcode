7fdc8f47920a1fd3c0b25aa0f55d97c638c7ecc5
张泽龙 <553234736@qq.com>
2020-03-07 17:34:06 +0800

    添加  	格雷编码(注释)

--- src/zzl/leetcode/GrayCode.java
+++ src/zzl/leetcode/GrayCode.java
@@ -31,8 +31,17 @@ public class GrayCode {
         for (int i = 0; i < count; i++) {
             // 格雷码的递推公式G(n) = B(n+1) XOR B(n)
             // G：格雷码 B：二进制码 n：正在计算的位
+            /*
+            1位格雷码有两个码字
+            (n+1)位格雷码中的前2n个码字等于n位格雷码的码字，按顺序书写，加前缀0
+            (n+1)位格雷码中的后2n个码字等于n位格雷码的码字，按逆序书写，加前缀1
+            n+1位格雷码的集合 = n位格雷码集合(顺序)加前缀0 + n位格雷码集合(逆序)加前缀1
+             */
             result.add(i ^ (i >> 1));
-            System.out.println(Integer.toBinaryString(i ^ (i >> 1)));
+            System.out.println(i >> 1);
+            System.out.println(i);
+            System.out.println(Integer.toBinaryString(i ^ (i + 1)));
+            System.out.println("=======");
         }
         return result;
     }
