2ca807c3ae6520d0427a060a56d7e6fc700e8c51
张泽龙 <553234736@qq.com>
2020-03-12 07:13:15 +0800

    添加  	爬楼梯(动态规划2.0)

--- src/zzl/leetcode/ClimbingStairs.java
+++ src/zzl/leetcode/ClimbingStairs.java
@@ -10,12 +10,36 @@ import org.junit.Assert;
  */
 public class ClimbingStairs {
     public static void main(String[] args) {
-        Assert.assertEquals(new ClimbingStairs().climbStairs(2),2);
-        Assert.assertEquals(new ClimbingStairs().climbStairs(3),3);
+        Assert.assertEquals(new ClimbingStairs().climbStairs(2), 2);
+        Assert.assertEquals(new ClimbingStairs().climbStairs(3), 3);
 
     }
 
+    /**
+     * 优化后的动态规划，只使用了较少的变量，不用保存整个dp数组
+     *
+     * @param n
+     * @return
+     */
     public int climbStairs(int n) {
+        int cur_i_1 = 1;
+        int temp_i_2 = 1;
+        int temp = 0;
+        for (int i = 2; i <= n; i++) {
+            temp = cur_i_1;
+            cur_i_1 = cur_i_1 + temp_i_2;
+            temp_i_2 = temp;
+        }
+        return cur_i_1;
+    }
+
+    /**
+     * 动态规划1.0
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairsDP(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
