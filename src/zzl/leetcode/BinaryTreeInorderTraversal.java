a396cee0f42107f26fc1d195940935ec29c0dd46
张泽龙 <553234736@qq.com>
2020-03-12 17:14:22 +0800

    添加  	二叉树的中序遍历(莫里斯遍历)

--- src/zzl/leetcode/BinaryTreeInorderTraversal.java
+++ src/zzl/leetcode/BinaryTreeInorderTraversal.java
@@ -24,12 +24,49 @@ public class BinaryTreeInorderTraversal {
     }
 
     /**
+     * 本方法中，我们使用一种新的数据结构：线索二叉树。方法如下：
+     * Step 1: 将当前节点current初始化为根节点
+     * Step 2: While current不为空，
+     * ----若current没有左子节点
+     * --------a. 将current添加到输出
+     * --------b. 进入右子树，亦即, current = current.right
+     * ----否则
+     * --------a. 在current的左子树中，令current成为最右侧节点的右子节点
+     * --------b. 进入左子树，亦即，current = current.left
+     *
+     * @param root
+     * @return
+     * @link {https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/er-cha-shu-de-zhong-xu-bian-li-by-leetcode/}
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        TreeNode current = root;
+        TreeNode pre;
+        while (current != null) {
+            if (current.left == null) {
+                result.add(current.val);
+                current = current.right;
+            } else {
+                pre = current.left;
+                while (pre.right != null)
+                    pre = pre.right;
+                pre.right = current;
+                TreeNode temp = current;
+                current = current.left;
+                temp.left = null;
+            }
+            PrintConsoleUtil.printTreeNode(root);
+        }
+        return result;
+    }
+
+    /**
      * 使用堆栈模拟递归
      *
      * @param root
      * @return
      */
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversalStack(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
         Stack<Integer> result = new Stack<>();
         TreeNode curNode = root;
