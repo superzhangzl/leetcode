a388f05b09e5c229d355f7c5491e18a06890c4b3
张泽龙 <553234736@qq.com>
2020-03-20 11:28:53 +0800

    添加  	相交链表(双指针)

--- src/zzl/leetcode/IntersectionOfTwoLinkedLists.java
+++ src/zzl/leetcode/IntersectionOfTwoLinkedLists.java
@@ -27,13 +27,44 @@ public class IntersectionOfTwoLinkedLists {
     }
 
     /**
-     * hash法：比较耗时
+     * 4 -> 1 -> 8 -> 4 -> 5 -> 5 -> 0 -> 1 -> 8 -> 4 -> 5
+     * 5 -> 0 -> 1 -> 8 -> 4 -> 5 -> 4 -> 1 -> 8 -> 4 -> 5
+     * ========================================|
+     *
+     * 创建两个指针 pApA 和 pBpB，分别初始化为链表 A 和 B 的头结点。然后让它们向后逐结点遍历。
+     * 当 pApA 到达链表的尾部时，将它重定位到链表 B 的头结点 (你没看错，就是链表 B); 类似的，当 pBpB 到达链表的尾部时，将它重定位到链表 A 的头结点。
+     * 若在某一时刻 pApA 和 pBpB 相遇，则 pApA/pBpB 为相交结点。
      *
      * @param headA
      * @param headB
      * @return
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode indexA = headA;
+        ListNode indexB = headB;
+        while (indexA != indexB) {
+            if (indexA == null) {
+                indexA = headB;
+            } else {
+                indexA = indexA.next;
+            }
+            if (indexB == null) {
+                indexB = headA;
+            } else {
+                indexB = indexB.next;
+            }
+        }
+        return indexA;
+    }
+
+    /**
+     * hash法：比较耗时
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNodeHash(ListNode headA, ListNode headB) {
         HashMap<ListNode, Integer> list = new HashMap<>();
         while (headA != null) {
             list.put(headA, 1);
