38bd6d4d9acc25fc978f86d3e4fd6c3c73670e92
张泽龙 <553234736@qq.com>
2020-03-23 16:14:30 +0800

    优化  	URL转换工具类

--- src/zzl/util/ConvertNameUtil.java
+++ src/zzl/util/ConvertNameUtil.java
@@ -11,14 +11,19 @@ public class ConvertNameUtil {
      * @param args
      */
     public static void main(String[] args) {
-        String url = "https://leetcode-cn.com/problems/move-zeroes/";
+        String url = "https://leetcode-cn.com/problems/jump-game-ii/";
         String replace = url.replace("https://leetcode-cn.com/problems/", "");
         String title = replace.substring(0, replace.length() - 1);
         String[] split = title.split("-");
         StringBuilder sb = new StringBuilder();
         for (String s : split) {
-            String s1 = s.substring(0, 1).toUpperCase() + s.substring(1);
-            sb.append(s1);
+            // 优化一下，出现多个连续i的时候，其实是罗马数字，这里适配到I~III
+            if (s.matches("i+")) {
+                sb.append(s.toUpperCase());
+            } else {
+                String s1 = s.substring(0, 1).toUpperCase() + s.substring(1);
+                sb.append(s1);
+            }
         }
         System.out.println();
         System.out.println("@link {" + url + "}");
