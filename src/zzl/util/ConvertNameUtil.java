df2d08242873fc014883d45e6c848fe6c1efc71f
zhangzelong <553234736@qq.com>
2020-11-28 14:30:38 +0800

    更新 工具方法

--- src/zzl/util/ConvertNameUtil.java
+++ src/zzl/util/ConvertNameUtil.java
@@ -11,7 +11,7 @@ public class ConvertNameUtil {
      * @param args
      */
     public static void main(String[] args) {
-        String url = "https://leetcode-cn.com/problems/find-the-duplicate-number/";
+        String url = "https://leetcode-cn.com/problems/daily-temperatures/";
         String replace = url.replace("https://leetcode-cn.com/problems/", "");
         String title = replace.substring(0, replace.length() - 1);
         String[] split = title.split("-");
