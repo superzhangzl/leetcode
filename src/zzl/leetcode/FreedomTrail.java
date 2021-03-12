1eb95fb6b2d48bcab69d3db0d10b90a06e3b9d7a
zhangzelong <553234736@qq.com>
2021-03-12 08:30:53 +0800

    更新 自由之路(添加TODO)

---
+++ src/zzl/leetcode/FreedomTrail.java
@@ -0,0 +1,18 @@
+package zzl.leetcode;
+
+/**
+ * 自由之路
+ * TODO
+ *
+ * @author zzl
+ * @link {https://leetcode-cn.com/problems/freedom-trail/}
+ */
+public class FreedomTrail {
+    public static void main(String[] args) {
+
+    }
+
+    public int findRotateSteps(String ring, String key) {
+        return -1;
+    }
+}
--- src/zzl/util/ConvertNameUtil.java
+++ src/zzl/util/ConvertNameUtil.java
@@ -11,7 +11,7 @@ public class ConvertNameUtil {
      * @param args
      */
     public static void main(String[] args) {
-        String url = "https://leetcode-cn.com/problems/longest-substring-with-at-least-k-repeating-characters/";
+        String url = "https://leetcode-cn.com/problems/sort-items-by-groups-respecting-dependencies/";
         String replace = url.replace("https://leetcode-cn.com/problems/", "");
         String title = replace.substring(0, replace.length() - 1);
         String[] split = title.split("-");
