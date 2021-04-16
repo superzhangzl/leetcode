fabe6f2cc404c2fd76a3f94136e7b049aa92e158
zhangzelong <553234736@qq.com>
2021-04-16 14:56:04 +0800

    更新 目标和(子集背包问题)

--- src/zzl/leetcode/TargetSum.java
+++ src/zzl/leetcode/TargetSum.java
@@ -1,5 +1,6 @@
 package zzl.leetcode;
 
+import org.junit.Assert;
 import zzl.util.GenerateUtil;
 import zzl.util.PrintConsoleUtil;
 
@@ -11,11 +12,77 @@ import zzl.util.PrintConsoleUtil;
  */
 public class TargetSum {
     public static void main(String[] args) {
-        int[] nums = GenerateUtil.generateIntArray("1, 1, 1, 1, 1");
+        int[] nums;
         TargetSum targetSum = new TargetSum();
-        int sumWays = targetSum.findTargetSumWays(nums, 3);
-        System.out.println(sumWays);
+        int sumWays;
+        //
+        nums = GenerateUtil.generateIntArray("1, 1, 1, 1, 1");
+        sumWays = targetSum.findTargetSumWays(nums, 3);
+        Assert.assertEquals(sumWays, 5);
+        //
+        nums = GenerateUtil.generateIntArray("0,0,0,0,0,0,0,0,1");
+        sumWays = targetSum.findTargetSumWays(nums, 1);
+        Assert.assertEquals(sumWays, 256);
 
+    }
+
+    /**
+     * 将目标和转换为完全背包问题
+     * SUM(A) - SUM(B) = TARGET
+     * SUM(A) = TARGET + SUM(B)
+     * SUM(A) = (TARGET + SUM(B)) / 2
+     * 即将问题转换为数组中存在几个子集A，是的A中元素和等于(TARGET + SUM(B)) / 2
+     *
+     * @param nums
+     * @param S
+     * @return
+     * @link {《labuladong的算法小抄》 P211}
+     */
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        // 特殊情况无法满足直接返回0
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        return fullBagDp(nums, (sum + S) / 2);
+    }
+
+    /**
+     * 当前不做一维压缩，不好理解
+     * TODO：待一维压缩，优化空间复杂度
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int fullBagDp(int[] nums, int target) {
+        int len = nums.length;
+        int[][] dp = new int[len + 1][target + 1];
+        for (int i = 0; i < len + 1; i++) {
+            dp[i][0] = 1;
+        }
+        /*
+        注意事项：每个数字只能被装一次，和完全背包有些区别
+        i从1开始，因为i等于0的时候已经被选择过来，已经为0了，且j的遍历依赖上一轮的操作
+         */
+        for (int i = 1; i < len + 1; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (j - nums[i - 1] >= 0) {
+                    /*
+                    dp为装满背包的总方法数：那就把两种情况对应的选择结果加起来（选择当前i还是不选择）
+                     */
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                } else {
+                    // 背包空间不足，只能选择不装
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        PrintConsoleUtil.printArray(dp);
+        return dp[len][target];
     }
 
     /**
@@ -24,7 +91,7 @@ public class TargetSum {
      * @return
      * @link {https://leetcode-cn.com/problems/target-sum/solution/dong-tai-gui-hua-si-kao-quan-guo-cheng-by-keepal/}
      */
-    public int findTargetSumWays(int[] nums, int S) {
+    public int findTargetSumWays_bad(int[] nums, int S) {
         /*
          */
         int sum = 0;
