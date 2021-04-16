05859c276ea70522e7154b49e5f9e63f660c6f5b
zhangzelong <553234736@qq.com>
2021-04-16 10:54:25 +0800

    新增 零钱兑换II(dp完全背包问题+空间优化)

--- src/zzl/leetcode/CoinChange2.java
+++ src/zzl/leetcode/CoinChange2.java
@@ -2,6 +2,7 @@ package zzl.leetcode;
 
 import org.junit.Assert;
 import zzl.util.GenerateUtil;
+import zzl.util.PrintConsoleUtil;
 
 /**
  * 零钱兑换 II
@@ -32,13 +33,53 @@ public class CoinChange2 {
     }
 
     /**
-     * dp：二维数组，未优化
+     * dp：一位数组进行空间压缩，并添加注释
      *
      * @param amount
      * @param coins
      * @return
      */
     public int change(int amount, int[] coins) {
+        int n = coins.length;
+        /*
+        dp定义：只使用前i个物品，当背包容量为j时，有dp[i][j]种方法装满背包。
+        即：使用前i个硬币，凑出金额j，有dp[i][j]种方法
+         */
+        int[] dp = new int[amount + 1];
+        // 注：金额为0，不使用硬币这一种解法，即使用硬币个数为0这一种解法，所以设置为1；
+        dp[0] = 1;
+        /*
+        每一个j只跟dp[i][]和dp[i-1][]有关，和j-1无关，将二维数组向下投影到一维即可实现状态压缩
+         */
+        System.out.print(String.format("[%2d]的面值 |", amount));
+        for (int i = 0; i < amount + 1; i++) {
+            System.out.print(String.format("%2d ", i));
+        }
+        System.out.println();
+        System.out.println("-----------------------------------------------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                // 此时金额j还没有通过前的i-1个硬币凑够
+                // i从1开始，coins[i-1]对应的是i的面值
+                if (j - coins[i] >= 0) {
+                    dp[j] = dp[j] + dp[j - coins[i]];
+                }
+            }
+            System.out.print(String.format("[%2d]种硬币 |", i + 1));
+            PrintConsoleUtil.printArray(dp);
+        }
+        System.out.println();
+        return dp[amount];
+    }
+
+    /**
+     * dp：二维数组，未优化
+     *
+     * @param amount
+     * @param coins
+     * @return
+     */
+    public int change_bad(int amount, int[] coins) {
         int n = coins.length;
         /*
         dp定义：只使用前i个物品，当背包容量为j时，有dp[i][j]种方法装满背包。
