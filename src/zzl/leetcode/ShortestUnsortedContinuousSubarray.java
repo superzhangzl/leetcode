9f439f83c345da638c619851d1c261e5f6cee083
zhangzelong <553234736@qq.com>
2021-08-03 21:01:59 +0800

    新增  最短无序连续子数(单调栈，性能略差)

--- src/zzl/leetcode/ShortestUnsortedContinuousSubarray.java
+++ src/zzl/leetcode/ShortestUnsortedContinuousSubarray.java
@@ -1,15 +1,23 @@
 package zzl.leetcode;
 
 import org.junit.Assert;
+import zzl.base.annotation.Level;
+import zzl.base.enums.Difficulty;
 import zzl.util.GenerateUtil;
 import zzl.util.PrintConsoleUtil;
 
 import java.util.Arrays;
+import java.util.Stack;
+
+import static zzl.base.enums.Difficulty.MEDIUM;
 
 /**
+ * 最短无序连续子数组
+ *
  * @author zzl
  * @link {https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/}
  */
+@Level(MEDIUM)
 public class ShortestUnsortedContinuousSubarray {
     public static void main(String[] args) {
         int[] array = GenerateUtil.generateIntArray("2,6,4,8,10,9,15");
@@ -21,6 +29,43 @@ public class ShortestUnsortedContinuousSubarray {
         array = GenerateUtil.generateIntArray("1");
         length = new ShortestUnsortedContinuousSubarray().findUnsortedSubarray(array);
         Assert.assertEquals(length, 0);
+        array = GenerateUtil.generateIntArray("2,1");
+        length = new ShortestUnsortedContinuousSubarray().findUnsortedSubarray(array);
+        Assert.assertEquals(length, 2);
+    }
+
+
+    /**
+     * 单调栈，
+     * 1. 升序遍历，先使得栈单调增，找到最小破坏栈递增的位置，
+     * 2. 降序遍历，再使得栈单调减，找到最大破坏栈递减的位置。
+     * 3. 用判断左右区间的长度
+     *
+     * @param nums
+     * @return
+     */
+    public int findUnsortedSubarray(int[] nums) {
+        // 单调栈，保存下标
+        Stack<Integer> stack = new Stack<>();
+        int left = 2;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[i] < nums[stack.peek()]) {
+                left = Math.min(left, stack.pop());
+            }
+            stack.push(i);
+        }
+        // 不能取min_value，否则return是做计算会出现下标越界的情况
+        int right = -1;
+        stack = new Stack<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
+                right = Math.max(right, stack.pop());
+            }
+            stack.push(i);
+        }
+        PrintConsoleUtil.printArray(nums);
+        PrintConsoleUtil.printParams("left", left, "right", right);
+        return Math.max(right - left + 1, 0);
     }
 
 
@@ -32,7 +77,7 @@ public class ShortestUnsortedContinuousSubarray {
      * @param nums
      * @return
      */
-    public int findUnsortedSubarray(int[] nums) {
+    public int findUnsortedSubarray_sort(int[] nums) {
         int[] copy = Arrays.copyOf(nums, nums.length);
         Arrays.sort(copy);
         PrintConsoleUtil.printArray(copy);
