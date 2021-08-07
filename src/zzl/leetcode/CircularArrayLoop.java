4ece962e8e1de18a7286f155eb048c1b27327cba
zhangzelong <553234736@qq.com>
2021-08-07 17:01:46 +0800

    更新  环形数组是否存在循环(拓扑排序)

--- src/zzl/leetcode/CircularArrayLoop.java
+++ src/zzl/leetcode/CircularArrayLoop.java
@@ -1,10 +1,13 @@
 package zzl.leetcode;
 
 import org.junit.Assert;
+import zzl.base.annotation.AlgTemplate;
 import zzl.base.annotation.Level;
 import zzl.util.GenerateUtil;
 
-import static zzl.base.enums.Difficulty.*;
+import java.util.*;
+
+import static zzl.base.enums.Difficulty.MEDIUM;
 
 /**
  * 环形数组是否存在循环
@@ -33,6 +36,102 @@ public class CircularArrayLoop {
     }
 
     /**
+     * 使用拓扑排序判断是否存在环
+     *
+     * @param nums
+     * @return
+     */
+    public boolean circularArrayLoop(int[] nums) {
+        int n = nums.length;
+        // 首先判断正向是否存在环
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegrees = new int[n];
+        // 初始化
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            // 找到当前节点指向的节点
+            int next = ((i + nums[i]) % n + n) % n;
+            // 如果方向是反向的先不管，
+            // 如果下一跳等于自己也不管，环的长度要大于1
+            if (nums[i] <= 0 || i == next) {
+                continue;
+            }
+            // 将当前节点的下一跳添加到图中
+            graph.get(i).add(next);
+            // 下一节点的入度+1
+            inDegrees[next]++;
+        }
+        if (hasLoopByTopoSort(graph, inDegrees, n)) {
+            return true;
+        }
+
+        // 再判断反向是否存在环
+        graph = new ArrayList<>();
+        Arrays.fill(inDegrees, 0);
+        // 初始化
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            // 找到当前节点指向的节点
+            int next = ((i + nums[i]) % n + n) % n;
+            // 如果方向是正向的先不管，前面已经算过了
+            // 如果下一跳等于自己也不管，环的长度要大于1
+            if (nums[i] >= 0 || i == next) {
+                continue;
+            }
+            // 将当前节点的下一跳添加到图中
+            graph.get(i).add(next);
+            // 下一节点的入度+1
+            inDegrees[next]++;
+        }
+        if (hasLoopByTopoSort(graph, inDegrees, n)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param graph
+     * @param inDegrees
+     * @param nodeCount
+     * @return
+     */
+    @AlgTemplate("拓扑排序")
+    private boolean hasLoopByTopoSort(List<List<Integer>> graph, int[] inDegrees, int nodeCount) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        // 首先将入度为0的节点写入算法
+        for (int i = 0; i < nodeCount; i++) {
+            if (inDegrees[i] == 0) {
+                queue.add(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            // 当前入度为0的点
+            int curNode = queue.poll();
+            // 找该节点指向的点
+            for (Integer node : graph.get(curNode)) {
+                // 将被指向的点的入度减一，即断开连接
+                inDegrees[node]--;
+                if (inDegrees[node] == 0) {
+                    // 将入度等于0的点放进队列
+                    queue.add(node);
+                }
+            }
+        }
+        for (int inDegree : inDegrees) {
+            if (inDegree != 0) {
+                // 还有节点的入度不等于0，说明有环
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
      * 暴力解法：
      * 因为不确定哪个点是起始点，所以需要假设以某一个点开始，算它是不是起点
      *
@@ -40,7 +139,7 @@ public class CircularArrayLoop {
      * @return
      * @link {https://leetcode-cn.com/problems/circular-array-loop/solution/gong-shui-san-xie-yi-ti-shuang-jie-mo-ni-ag05/}
      */
-    public boolean circularArrayLoop(int[] nums) {
+    public boolean circularArrayLoop_Bad(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             if (checkIsLoopStartPoint(i, nums)) {
                 return true;
